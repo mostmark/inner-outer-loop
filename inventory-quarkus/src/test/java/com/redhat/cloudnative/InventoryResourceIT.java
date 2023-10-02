d5deb74a2036149c0203cd14d3184c725ee78c37
mostmark <martin@ostmarks.se>
2023-10-02 10:56:36 +0200

    Corrected test class names

--- inventory-quarkus/src/test/java/com/redhat/cloudnative/InventoryResourceIT.java
+++ inventory-quarkus/src/test/java/com/redhat/cloudnative/InventoryResourceIT.java
@@ -3,6 +3,6 @@ package com.redhat.cloudnative;
 import io.quarkus.test.junit.QuarkusIntegrationTest;
 
 @QuarkusIntegrationTest
-public class GreetingResourceIT extends GreetingResourceTest {
+public class InventoryResourceIT extends InventoryResourceTest {
     // Execute the same tests but in packaged mode.
 }
